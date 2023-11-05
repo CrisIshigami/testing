f7978fc85bae258c52390b54220d3e2587eb500c
CrisIshigami <140532305+CrisIshigami@users.noreply.github.com>
2023-11-04 22:09:15 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+const menu=document.querySelector('.img_menu');
+const menuList=document.querySelector('.ul__nav');
+const link=document.querySelectorAll('.nav__link');
+
+menu.addEventListener('click',function(){
+    menuList.classList.toggle('ul__nav--show');
+
+    if (menuList.classList.contains('ul__nav--show')) {
+        menu.src = '/images/menu_cancelar.png';
+    } else {
+    menu.src = '/images/barra-de-menus.png';
+    }
+    
+});
+
+link.forEach(function(link){
+    link.addEventListener('click',function(){
+        menuList.classList.remove('ul__nav--show');
+        menu.src = '/images/barra-de-menus.png';
+    })
+});
+
+
+/*----ANIMACIÓN AL HACER SCROLL*/
+
+const sectionInformation= document.querySelector('.section__information');
+
+const loadSection = (entries, observer)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            console.log('La sección esta en el viewport');
+        }
+        else{
+            console.log('No hay NADA')
+        }
+    })
+}
+
+const observe= new IntersectionObserver(loadSection,{
+    root: null,
+    rootMargin: '0px',
+    threshold: 1.0
+});
+
+observe.observe(sectionInformation);
